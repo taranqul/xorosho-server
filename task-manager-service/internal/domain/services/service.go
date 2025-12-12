276bab6a5dae4c338230af04a5e3bce0eb7dadc6
Taranqul <inbocs48@gmail.com>
2025-12-12 03:51:13 +0300

    feat: added status changing

--- task-manager-service/internal/domain/services/service.go
+++ task-manager-service/internal/domain/services/service.go
@@ -20,15 +20,39 @@ func NewTaskService(task_repository repositories.TaskRepositoryInterface, logger
 	}, nil
 }
 
-func (s *TaskService) CreateTask(task domain.Task) (uuid.UUID, error) {
+func (s *TaskService) CreateTask(task domain.Task) (*uuid.UUID, error) {
+	ready_to_init := false
+	status := "idle"
+
+	for i := range task.Objects {
+		task.Objects[i] = ""
+	}
+
+	if len(task.Objects) == 0 {
+		status = "in progress"
+		ready_to_init = true
+	}
+
 	to_create := domain.TaskInRepository{
 		Id:      task.Id.String(),
 		Type:    task.Type,
 		Objects: task.Objects,
 		Payload: task.Payload,
-		Status:  "idle",
+		Status:  status,
 	}
-	return s.task_repository.Create(&to_create)
+	uuid, err := s.task_repository.Create(&to_create)
+
+	if err != nil {
+
+		s.logger.Sugar().Errorf("task wasnt created: %v", to_create.Id)
+		return nil, err
+	}
+
+	if ready_to_init {
+		s.initTask(&to_create)
+	}
+
+	return &uuid, nil
 }
 
 func (s *TaskService) GetTaskStatus(id string) (*string, error) {
@@ -43,9 +67,30 @@ func (s *TaskService) HandleUploadedFile(uploaded_file domain.UploadedFilesMessa
 	}
 
 	task.Objects[uploaded_file.Type] = uploaded_file.File
+	ready_to_init := true
+
+	for _, v := range task.Objects {
+		if v == "" {
+			ready_to_init = false
+			break
+		}
+	}
+	if ready_to_init {
+		task.Status = "In progress"
+	}
+
 	err = s.task_repository.Put(*task)
 	if err != nil {
-		s.logger.Sugar().Errorf("uploaded file with %v wasnt putted because of %v, but was commited (thats actualy very bad)", uploaded_file.File, err)
+		s.logger.Sugar().Errorf("uploaded file with %v wasnt putted because of %v, but was commited (thats actualy very bad) and marked as 'in progres' (even worse)", uploaded_file.File, err)
 		return
 	}
+
+	if ready_to_init {
+		s.initTask(task)
+	}
+}
+
+func (s *TaskService) initTask(task *domain.TaskInRepository) error {
+	s.logger.Sugar().Infof("not implemented yet: %v", task.Id)
+	return nil
 }
